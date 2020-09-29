8c21c5d1ac94b18945cb3ef93292de73c8128f46
KimJaeHwan96 <lazysoul14@gmail.com>
2020-09-29 15:45:42 +0900

    lists/static 추가

--- superlists/settings.py
+++ superlists/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'lists/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
