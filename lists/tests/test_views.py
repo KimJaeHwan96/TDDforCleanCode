25fa15ba2d1dd39b971d4d2852a0a8f52eeaac5a
KimJaeHwan96 <lazysoul14@gmail.com>
2020-10-10 14:42:27 +0900

    모델 계층의 중복 아이템 검증 구현

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,7 +7,7 @@ import re
 from lists.models import Item, List
 from lists.forms import ItemForm
 from django.utils.html import escape
-import json
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 class HomePageTest(TestCase):
     """
     maxDiff =None
@@ -97,6 +97,36 @@ class ListViewTest(TestCase):
         expected_error = "빈 아이템을 등록할 수 없습니다"
         self.assertContains(response, expected_error)
 
+    def test_display_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
+
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'text': ''}
+        )
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, EMPTY_LIST_ERROR)
+
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
         self.client.post(
@@ -117,15 +147,20 @@ class NewListTest(TestCase):
         #self.assertEqual(response.status_code, 302)
         #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = "빈 아이템을 등록할 수 없습니다"
-        self.assertContains(response, expected_error)
-
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, EMPTY_LIST_ERROR)
+
+    def test_for_invalid_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
